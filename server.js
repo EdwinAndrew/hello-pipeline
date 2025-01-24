43840e7e5bcd49f277492025d73e8fb32c199bd8
Edwin Andrew <edwinandrew96@gmail.com>
2025-01-23 19:44:39 -0500

    Updated k8s manifests

--- server.js
+++ server.js
@@ -2,16 +2,17 @@ const http = require('node:http');
 
 const hostname = '0.0.0.0';
 const port = process.env.PORT || 3000;
+const podName = process.env.POD_NAME || 'unknown';
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, World!\n');
+  res.end('Hello, World! This response is from pod: ${podName}\n');
 });
 
 if (require.main === module) {
   server.listen(port, hostname, () => {
-    console.log(`Server running on port ${port}`);
+    console.log(`Server running on port ${port} in pod ${podName}`);
   });
 }
 
